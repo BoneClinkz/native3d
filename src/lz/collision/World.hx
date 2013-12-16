e5afb6eb4ebcf7d1e830a96dcf8b8a253c4adc96
matrix3d <405628079@qq.com>
2013-12-16 13:46:30 +0800

    fixed the issue #9

--- src/lz/collision/World.hx
+++ src/lz/collision/World.hx
@@ -106,10 +106,12 @@ class World extends EventDispatcher
 		}
 		for (box in boxs) {
 			if (box.collidablePairs!=null) {
-				box.collidablePairs.sort(function(x:CollidablePair, y:CollidablePair):Int {return x.timeToCollision < y.timeToCollision?1: -1; } );
+				box.collidablePairs.sort(sortcollidablePairsFun);
 			}
 		}
 	}
 	
-	
+	private function sortcollidablePairsFun(x:CollidablePair, y:CollidablePair):Int {
+		return x.timeToCollision < y.timeToCollision?1: -1; 
+	} 
 }
